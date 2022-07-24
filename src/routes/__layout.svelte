c179635238d5ea006a3d71aff18f8958a0feb85c
Ricky Rivas <rickyrivas918@gmail.com>
2022-07-24 00:52:06 -0500

    import contentful bug fix

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { browser } from '$app/env';
+</script>
+
+<svelte:head>
+	{#if browser}
+		<script
+			src="https://cdn.jsdelivr.net/npm/contentful@latest/dist/contentful.browser.min.js"
+			defer></script>
+	{/if}
+</svelte:head>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,4 @@
 <script context="module">
-	// builds correctly but doesnt run in dev environment
-	// import contentful from 'contentful';
-	// const { createClient } = contentful;
-
-	// works in dev environment but fails to build
-	// import * as contenful from 'contentful';
-
 	// let client = contentful.createClient({
 	// 	space: import.meta.env.VITE_SPACE_ID,
 	// 	accessToken: import.meta.env.VITE_CONTENTFUL_PUBLIC_TOKEN
@@ -33,11 +26,36 @@
 	// }
 </script>
 
-<!-- <script>
-	export let products;
+<script>
+	import { onMount } from 'svelte';
+	onMount(() => {
+		let client = contentful.createClient({
+			space: import.meta.env.VITE_SPACE_ID,
+			accessToken: import.meta.env.VITE_CONTENTFUL_PUBLIC_TOKEN
+		});
+
+		let products = [];
+
+		async function load() {
+			await client.getEntries().then((res) =>
+				res.items.forEach((item) => {
+					const { title, price } = item.fields;
+					const { id } = item.sys;
+					const product = {
+						title,
+						price,
+						id
+					};
+					products.push(product);
+				})
+			);
+			console.log(products);
+		}
+		load();
+	});
 </script>
 
-{#each products as product}
+<!-- {#each products as product}
 	<h1>{product.title}</h1>
 	<p>{product.price}</p>
 	<a href={'/products/' + product.id}>View {product.title}</a>
